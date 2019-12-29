f466b9925947b92d1f7f2773780b76acf2ff48c7
jipizarroo <jipizarroo@gmail.com>
2019-12-29 18:02:04 -0300

    avanzando

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Navbar = (props) =>{
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                      <div className="container">
+                <div className="row ml-1">
+                    <div className="col-md-12"></div>
   <a className="navbar-brand" href="#">Navbar</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -35,6 +38,8 @@ const Navbar = (props) =>{
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
       <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+  </div>
+  </div>
   </div>
 </nav>
     )
